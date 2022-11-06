813a0607eb09994894a8084682a015c0cbbc3821
Faycel <faycel.sassi@esen.tn>
2022-11-06 19:48:04 +0100

    CSS Update

--- src/app/Property/property-card/property-card.ts
+++ src/app/Property/property-card/property-card.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-property-card',
@@ -9,10 +9,6 @@ import { Component } from "@angular/core";
 }
 )
 export class PropertyCardComponent{
-  Property: any ={
-    "Id":1,
-    "name":"Iron-Man",
-    "type":"Avenger",
-    "ability":"strong af"
-  }
+  @Input()Property :any
+
 }
